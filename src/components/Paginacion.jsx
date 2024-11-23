29a43e71bc763fd35ef7ea5cc8bc63dfd1b0cfd8
RekZNoZ <rafikicroz@hotmail.com>
2024-11-24 00:08:45 +0100

    Create Paginacion.jsx

---
+++ src/components/Paginacion.jsx
@@ -0,0 +1,46 @@
+import "../css/paginacion.css"
+
+export default function Paginacion( atributos ) {
+
+    const { pagina, totalPaginas, paginacion, pageNumbers, handlePageInput } = atributos
+
+    return (
+        <nav className="paginacion">
+            <button
+                disabled={pagina === 1}
+                onClick={() => paginacion(pagina - 1)}
+            >
+                Previous
+            </button>
+            {pageNumbers.map(pageNumber => (
+                <button
+                    key={pageNumber}
+                    className={pagina === pageNumber ? "active" : ""}
+                    onClick={() => paginacion(pageNumber)}
+                >
+                    {pageNumber}
+                </button>
+            ))}
+            <button
+                disabled={pagina === totalPaginas}
+                onClick={() => paginacion(pagina + 1)}
+            >
+                Next
+            </button>
+
+            {/* Input para ir a una página directamente */}
+            <div className="page-input">
+                <input
+                    type="number"
+                    value={pagina}
+                    min="1"
+                    max={totalPaginas}
+                    onChange={handlePageInput}
+                    aria-label="Go to page"
+                />
+                <span> / {totalPaginas}</span>
+            </div>
+
+        </nav>
+    )
+}
