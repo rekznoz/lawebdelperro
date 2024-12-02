229acae33bfd784608b425e618dad5852320414a
RekZNoZ <rafikicroz@hotmail.com>
2024-12-02 13:54:53 +0100

    Intento de carrusel

---
+++ src/config/Imagenes_carrusel.jsx
@@ -0,0 +1,9 @@
+import perro_1 from '../assets/carrusel/perro_1.png'
+import perro_2 from '../assets/carrusel/perro_2.png'
+import perro_3 from '../assets/carrusel/perro_3.png'
+
+export const imagenes_carrusel = [
+    perro_1,
+    perro_2,
+    perro_3,
+]
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,14 +2,42 @@ import '../css/inicio.css'
 
 // import {} from '../config/FirebaseDB.jsx'
 
+import '../css/carrusel.css'
+import {imagenes_carrusel} from "../config/Imagenes_carrusel.jsx";
+import {useState} from "react";
+
 export default function Inicio() {
+
+    const images = imagenes_carrusel;
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const goToPrevious = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    };
+
+    const goToNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    };
+
     return (
         <>
             <div id='area1'>
-                <h1>Inicio</h1>
+                <div className="carousel">
+                    <button className="carousel-button prev" onClick={goToPrevious}>
+                        &#10094;
+                    </button>
+                    <div className="carousel-image">
+                        <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`}/>
+                    </div>
+                    <button className="carousel-button next" onClick={goToNext}>
+                        &#10095;
+                    </button>
+                </div>
             </div>
             <div id='area2'>
                 <h1>oicini</h1>
+                {/* Seccion de herramientas */}
             </div>
             <div id='area3'>
                 <h1>Home</h1>
